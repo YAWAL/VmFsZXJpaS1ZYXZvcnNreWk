328341e945b609d94568d56de98a50d5867ae7f2
YAWAL <yavorskyyval@gmail.com>
2019-09-24 09:56:05 +0300

    add routers

---
+++ src/router/routes.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/YAWAL/VmFsZXJpaS1ZYXZvcnNreWk/src/database"
+	"github.com/YAWAL/VmFsZXJpaS1ZYXZvcnNreWk/src/handlers"
+	"github.com/YAWAL/VmFsZXJpaS1ZYXZvcnNreWk/src/worker"
+
+	"github.com/gorilla/mux"
+)
+
+func InitRouter(er database.Repository, w worker.Worker) (r *mux.Router) {
+	r = mux.NewRouter()
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/fetcher", handlers.SaveURLData(er, w)).Methods(http.MethodPost)
+	api.HandleFunc("/fetcher", handlers.GetAllURLData(er)).Methods(http.MethodGet)
+	api.HandleFunc("/fetcher/{id}", handlers.DeleteURLData(er)).Methods(http.MethodDelete)
+	api.HandleFunc("/fetcher/{id}/history", handlers.GetDownloadHistoriesByURLDataID(er)).Methods(http.MethodGet)
+	return r
+}
